6259e578038e63a7c60545b2eba3cecc6a43d939
Bahareh <bahareh.adham@gmail.com>
2018-08-18 22:12:53 -0400

    almost done

--- script.js
+++ script.js
@@ -4,8 +4,6 @@ const houses = [
      {
         name: "Lannister",
         background: ".lan-red",
-        picture: "assets/cersei.jpg",
-        alt: "Cercei meme mocking her alcoholism",
         text: "You're a Lannister! Congratulations on being terrible!",
         color: ".lan-gold",
         points: 0-15
@@ -13,8 +11,6 @@ const houses = [
    {
         name: "Baratheon",
         background: ".bar-yellow",
-        picture: "assets/stannis.jpg",
-        alt: "Stannis meme making fun of his death",
         text: "You're a Baratheon!You ain't so bad, but your whole line is dead! Congrats!",
         color: "bar-black",
         points: 16-25
@@ -22,8 +18,6 @@ const houses = [
     {
         name: "Targaryen",
         background: "targ-black",
-        picture: "assets/danny.png",
-        alt: "Daenerys meme laughing at how she burns people alive",
         text: "You're a Targaryen! You're a lizard person and no one can spell your name. Yay!",
         color: "targ-red",
         points: 26-35
@@ -32,8 +26,6 @@ const houses = [
     {
         name: "Stark",
         background: "stark-green",
-        picture: "assets/ned.jpg",
-        alt: "Ned Stark meme talking about how he's dead",
         text: "You're a Stark and everybody picks on you! Congrats!",
         color: "stark-darkgrey",
         points: 36-45
@@ -42,8 +34,6 @@ const houses = [
     {
         name: "Greyjoy",
         background: "grey-black",
-        picture: "assets/theon.jpeg",
-        alt: "Theon meme making fun of his unfortunate fate",
         text: "You're a Greyjoy! Sadly there's no joy to being a Greyjoy. Good for you!",
         color: "grey-yellow",
         points: 46-56
@@ -102,13 +92,13 @@ $(function() {
         const printToPage = function(family) {
             $('.results-wrapper').empty();
             let header = $('<h1>').text(family.name); // make an h1 element with the house name in it
-            let image = $('<img>').attr('src', family.picture).attr('alt', family.alt);
+            let image = $('<img>').attr('src', "assets/dragon.svg");
             let text = $('<p>').text(family.text).addClass(family.color);
-            let reTry = `<div class="tryAgain"><a href=""><p>Don't like what you see? Try again!</p></a></div>`;
+            let reTry = `<div class="tryAgain button animated infinite pulse"><p><a href="">Don't like what you see? Try again!</a></p></div>`;
 
-            $('.results-wrapper').append(header, image, text, reTry).addClass(family.background).addClass('page');
+            // $('.your-results').addClass('page').append(image);
 
-
+            $('.results-wrapper').append(header, text, reTry);
 
 
             $('html, body').animate({
