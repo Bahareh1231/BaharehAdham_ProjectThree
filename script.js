c7f8f610eb5eae3d07add5c57ff62f8a7a5d350e
Bahareh <bahareh.adham@gmail.com>
2018-08-19 21:44:55 -0400

    final push

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const houses = [
         name: "Lannister",
         background: ".lan-red",
         text: "You're a Lannister! Congratulations on being terrible!",
+        pic: "assets/lion.svg",
         color: ".lan-gold",
         points: 0-15
     },
@@ -12,6 +13,7 @@ const houses = [
         name: "Baratheon",
         background: ".bar-yellow",
         text: "You're a Baratheon!You ain't so bad, but your whole line is dead! Congrats!",
+        pic: "assets/stag.svg",
         color: "bar-black",
         points: 16-25
     },
@@ -19,6 +21,7 @@ const houses = [
         name: "Targaryen",
         background: "targ-black",
         text: "You're a Targaryen! You're a lizard person and no one can spell your name. Yay!",
+        pic: "assets/dragon2.svg",
         color: "targ-red",
         points: 26-35
 
@@ -27,6 +30,7 @@ const houses = [
         name: "Stark",
         background: "stark-green",
         text: "You're a Stark and everybody picks on you! Congrats!",
+        pic: "assets/wolf.svg",
         color: "stark-darkgrey",
         points: 36-45
 
@@ -35,6 +39,7 @@ const houses = [
         name: "Greyjoy",
         background: "grey-black",
         text: "You're a Greyjoy! Sadly there's no joy to being a Greyjoy. Good for you!",
+        pic: "assets/squid.svg",
         color: "grey-yellow",
         points: 46-56
 
@@ -92,13 +97,13 @@ $(function() {
         const printToPage = function(family) {
             $('.results-wrapper').empty();
             let header = $('<h1>').text(family.name); // make an h1 element with the house name in it
-            let image = $('<img>').attr('src', "assets/dragon.svg");
+            let image = $('<img>').attr('src', family.pic);
             let text = $('<p>').text(family.text).addClass(family.color);
-            let reTry = `<div class="tryAgain button animated infinite pulse"><p><a href="">Don't like what you see? Try again!</a></p></div>`;
+            let reTry = `<div class="tryAgain button animated infinite pulse"><p><a href="">Try again</a></p></div>`;
 
-            // $('.your-results').addClass('page').append(image);
+            $('.your-results').addClass('page');
 
-            $('.results-wrapper').append(header, text, reTry);
+            $('.results-wrapper').append(header, image, text, reTry);
 
 
             $('html, body').animate({
@@ -127,7 +132,7 @@ $(function() {
             let result = houses[3]
             printToPage(result);
 
-        } else if(totalScore >= 46 && totalScore <= 55) {
+        } else if(totalScore >= 46 && totalScore <= 56) {
             let result = houses[4]
             printToPage(result);
 
